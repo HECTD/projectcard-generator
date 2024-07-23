8686728a92ae3b7b4a256958c8bf33c6f8ba8499
UHAsikakutou <91722200+UHAsikakutou@users.noreply.github.com>
2024-07-23 19:40:44 +0900

    Fix: cannot load svg (3)

--- src/hooks/getCard.ts
+++ src/hooks/getCard.ts
@@ -3,12 +3,15 @@ export default async function getCard(): Promise<SVGSVGElement> {
     `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"></svg>`,
     "image/svg+xml"
   ).childNodes[0] as SVGSVGElement;
-  await fetch(`/projectcard-generator/assets/card2.svg`).then((res) => {
+  await fetch(
+    `https://raw.githubusercontent.com/HECTD/projectcard-generator/main/public/card2.svg`
+  ).then((res) => {
     const newHeaders = new Headers(res.headers);
     newHeaders.set("Access-Control-Allow-Origin", "*");
     res.text().then((svg) => {
       console.log("svg", svg);
-      fetched = new DOMParser().parseFromString(svg, "image/svg+xml")
+      const unEscapedSvg = svg.replace(/&quot;/g, '"');
+      fetched = new DOMParser().parseFromString(unEscapedSvg, "image/svg+xml")
         .childNodes[0] as SVGSVGElement;
     });
   });
